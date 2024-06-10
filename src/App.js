dd1701bcbf638e957f7a84b78031949141493481
Dmitrii Dolikhin <haxall53@gmail.com>
2024-06-10 20:46:40 +0300

    add animation, creat Myhook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 // import Counter from './components/Counter';
 // import ClassCounter from './components/ClassCounter';
 // import PostItem from './components/PostItem';
@@ -10,6 +10,7 @@ import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/myModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -20,20 +21,7 @@ function App() {
   
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
-
-
-const sortedPosts = useMemo(() => {
-  console.log('function worging');
-  if (filter.sort) {
-    return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-  }
-  return posts;
-}, [filter.sort, posts])
-
-const sortedAnsSearchedPosts = useMemo(() => {
-  
-  return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-}, [filter.query, sortedPosts])
+  const sortedAnsSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
